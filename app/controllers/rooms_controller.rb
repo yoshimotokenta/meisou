def08835bb5eee9a60a632a30e6bdd6f6802831e
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-12-01 09:15:47 +0900

    新規登録機能の実装【ビュー修正】

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,11 @@
 class RoomsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Room.create(room_params)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'rooms/index'
   devise_for :users
   root to: "rooms#index"
+  resources :rooms, only: [:index, :new, :create]
 end
