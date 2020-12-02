8df50df7b9ea3f5aad69f840a8cdab25a64b465f
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-12-02 14:39:26 +0900

    サイドバーフロント作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,13 @@
 class Room < ApplicationRecord
-end
+    has_many :room_users
+    has_many :users
+    has_many :messages
+  
+  
+    validates :name,presence:true
+    validates :when,presence:true
+    validates :where,presence:true
+    validates :who,presence:true
+    validates :why,presence:true
+    validates :how,presence:true
+  end
